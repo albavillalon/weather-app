a3ce93e5f81a608f92e9010fb3dc9fb52a0ed4bb
Alba <albavillalonlucas@gmail.com>
2019-01-13 19:02:57 +0100

    Añadida la libreria de weather icons + Paso de parametros entre componentes + creacion del componente WeatherTemperature + constantes

--- src/components/WeatherData.js
+++ src/components/WeatherData.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import WeatherExtraInfo from './WeatherExtraInfo';
 import WeatherTemperature from './WeatherTemperature';
-
+import {
+    CLOUD, CLOUDY, SUN, RAIN, SNOW, WINDY,
+} from './../constants/weather';
 const WeatherData = () => (
     <div>
-        <WeatherTemperature />
-        <WeatherExtraInfo />
+        <WeatherTemperature 
+            temperature={20} 
+            weatherState={SUN}
+        />
+        <WeatherExtraInfo humidity ={80} wind={"10 m/s"}/>
     </div>
 );
 
--- src/components/WeatherTemperature.js
+++ src/components/WeatherTemperature.js
@@ -1,7 +1,36 @@
 import React from 'react';
-
-const WeatherTemperature = () => (
-    <div><span>25º</span></div>
+import WeatherIcons from 'react-weathericons';
+import {
+    CLOUD, CLOUDY, SUN, RAIN, SNOW, WINDY,
+} from './../constants/weather';
+import PropTypes from 'prop-types';
+const icons = {
+    [CLOUD]: "cloud",
+    [CLOUDY]: "cloudy",
+    [SUN]: "day-sunny",
+    [RAIN]: "rain",
+    [SNOW]: "snow",
+    [WINDY]: "windy"
+};
+const getWeatherIcon = (weatherState) => {
+    const icon = icons[weatherState];
+    if (icon)
+        return <WeatherIcons name={icon} size="2x" />;
+    else
+        return <WeatherIcons name="day-sunny" size="2x" />;
+}
+const WeatherTemperature = ({ temperature, weatherState}) => (
+    <div>
+        {
+            getWeatherIcon(weatherState)
+        }
+        <span>{`${temperature} ºC`}</span>
+    </div>
 );
+
+WeatherTemperature.propTypes = {
+    temperature: PropTypes.number,
+    weatherState: PropTypes.string.isRequired,
+};
 
 export default WeatherTemperature;
